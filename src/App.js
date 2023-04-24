954e2f15d4e0878a34917e2041b4b94e2a85ae44
shivam567890 <shivam.kh007@gmail.com>
2023-04-24 17:37:46 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ const App=()=> {
         onLoaderFinished={() => (setProgress(0))}
       />
         <Routes>
-        <Route exact  path='/' element={ <News setProgress={setProgress}  key="Current News" country="us" category="general"/>}/>
-          <Route exact  path='/general' element={ <News setProgress={setProgress}  key="general" country="us" category="general"/>}/>
-          <Route exact  path='/science' element={ <News setProgress={setProgress}  key="science" country="us" category="science"/>}/>
-          <Route exact  path='/business' element={ <News setProgress={setProgress}  key="business" country="us" category="business"/>}/>
+        <Route exact  path='/' element={ <News setProgress={setProgress}  key="Current News" country="in" category="general"/>}/>
+          <Route exact  path='/general' element={ <News setProgress={setProgress}  key="general" country="in" category="general"/>}/>
+          <Route exact  path='/science' element={ <News setProgress={setProgress}  key="science" country="in" category="science"/>}/>
+          <Route exact  path='/business' element={ <News setProgress={setProgress}  key="business" country="in" category="business"/>}/>
           <Route exact  path='/health' element={ <News setProgress={setProgress}  key="health" country="in" category="health"/>}/>
-          <Route exact  path='/entertainment' element={ <News setProgress={setProgress}  key="entertainment" country="us" category="entertainment"/>}/>
-          <Route exact  path='/technology' element={ <News setProgress={setProgress}  key="technology" country="us" category="technology"/>}/>
-          <Route exact  path='/sports' element={ <News setProgress={setProgress}  key="sports" country="us" category="sports"/>}/>
+          <Route exact  path='/entertainment' element={ <News setProgress={setProgress}  key="entertainment" country="in" category="entertainment"/>}/>
+          <Route exact  path='/technology' element={ <News setProgress={setProgress}  key="technology" country="in" category="technology"/>}/>
+          <Route exact  path='/sports' element={ <News setProgress={setProgress}  key="sports" country="in" category="sports"/>}/>
              </Routes>
              </Router>
       </div>
